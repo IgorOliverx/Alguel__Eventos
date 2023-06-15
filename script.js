9c9f93acc675958c658c093fcd831c2c6dd14e5c
IgorOliverx <123770573+IgorOliverx@users.noreply.github.com>
2023-06-15 18:20:21 +0000

    commit4

---
+++ script.js
@@ -0,0 +1,20 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+
+    var emailInput = document.getElementById('inputEmail');
+    var passwordInput = document.getElementById('inputPassword');
+
+    var email = emailInput.value;
+    var password = passwordInput.value;
+
+    if (email === '') {
+      alert('Por favor, preencha o campo de e-mail.');
+    } else if (password === '') {
+      alert('Por favor, preencha o campo de senha.');
+    } else if (!password.match(passwordRegex)) {
+        alert('A senha deve conter no mínimo 8 caracteres, pelo menos 1 letra maiúscula, 1 número e 1 caractere especial.');
+      } else { 
+      document.getElementById('loginForm').submit();
+    }
+  }
+  );
